e439e98fac75fd2ea39978c4488d23ef4be640bd
vasu.senawongse <vasu@coursesquare.co>
2021-06-27 23:02:43 +0700

    add connect DB test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,19 @@
 const { getProduct } = require('../services/BaseService');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+it('should connect to Mongo DB', async () => {
+  mongoose.connect(
+    `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?readPreference=primary&appname=MongoDB%20Compass&ssl=false`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+
+  setTimeout(function () {
+    if (mongoose.connection.readyState == 1) {
+      expect(mongoose.connection.readyState).toEqual(1);
+    }
+  }, 5000);
+});
 
 it('should be able to get Base Sum Assured from FWD', async () => {
   const result = await getProduct({
